aaa7ecf1eb3525fb8c0e0e2605cb5e92157ac48d
mikiyasmebrate <mikiyasmebrate@gmail.com>
2024-07-29 15:56:40 +0300

    Nav Bar added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import "./assets/css/app.min.css"
+import "./assets/css/bootstrap.min.css"
+import "./assets/css/icons.min.css"
+
+import NavBar from './components/ui/NavBar'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <div class="layout-wrapper d-lg-flex">
+        <NavBar />
+     </div>
   </React.StrictMode>,
 )
